4e9b3a208c3d7a56388a60e758756ec754029d12
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-01-10 10:20:11 -0700

    Create memprint.h

---
+++ memprint.h
@@ -0,0 +1,8 @@
+void memprint_chars( char *mem );
+void memprint_chars( char *mem, int num_bytes );
+
+void memprint_bytes( uint8_t *mem );
+void memprint_bytes( uint8_t *mem, int num_bytes );
+
+void memprint_CRGB( CRGB *mem );
+void memprint_CRGB( CRGB *mem, int num_bytes );
