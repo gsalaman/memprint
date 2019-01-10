b45898653f6bba88ef65b5fbbe4667ad6e059c9e
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-01-10 11:24:21 -0700

    flushing out functions

--- memprint.cpp
+++ memprint.cpp
@@ -1,7 +1,11 @@
+#include <Arduino.h>
+#include "memprint.h"
+#include "SoftwareSerial.h"
+
 void memprint_chars( char *mem, int num_bytes)
 {
   int i;
-  uint8_t myByte;
+  unsigned char myByte;
   char tmp_str[40];
 
   Serial.println("Addr\thex\tint\tchar");
@@ -16,6 +20,7 @@ void memprint_chars( char *mem, int num_bytes)
     mem++;
   }
 
+  Serial.println("=====================");
 }
 
 void memprint_chars( char *mem )
@@ -23,10 +28,20 @@ void memprint_chars( char *mem )
   memprint_chars( mem, 10);
 }
 
-void memprint_bytes( uint8_t *mem, int num_bytes)
+void memprint_chars( void *mem )
+{
+  memprint_chars( (char *) mem, 10);
+}
+
+void memprint_chars( void *mem, int num_chars )
+{
+  memprint_chars( (char *) mem, num_chars);
+}
+
+void memprint_bytes( unsigned char *mem, int num_bytes)
 {
   int i;
-  uint8_t myByte;
+  unsigned char myByte;
   char tmp_str[40];
 
   Serial.println("Addr\thex\tint");
@@ -40,9 +55,82 @@ void memprint_bytes( uint8_t *mem, int num_bytes)
     mem++;
   }
 
+  Serial.println("=====================");
 }
 
-void memprint_bytes( uint8_t *mem )
+void memprint_bytes( unsigned char *mem )
 {
   memprint_bytes( mem, 16);
+}
+
+void memprint_bytes( void *mem )
+{
+  memprint_bytes( (unsigned char * ) mem, 16);
+}
+
+void memprint_bytes( void *mem, int num_bytes )
+{
+  memprint_bytes( (unsigned char *) mem, num_bytes);
+}
+
+void memprint_ints( int *mem, int num_ints)
+{
+  int i;
+  int integer;
+  char tmp_str[40];
+
+  Serial.println("Addr\tint\thex");
+
+  for (i = 0; i < num_ints; i++)
+  {
+    integer = *mem;
+
+    sprintf(tmp_str, "0x%04x %5d 0x%04x", (unsigned int) mem, integer, integer);
+    Serial.println(tmp_str);
+
+    mem++;
+  }
+
+  Serial.println("=====================");
+
+}
+
+void memprint_ints( int *mem )
+{
+  memprint_ints(mem, 10);
+}
+
+void memprint_CRGB( void *mem, int num)
+{
+  int i;
+  int j;
+  char tmp_str[40];
+  char *byte_ptr;
+  
+  Serial.println("Addr\tR\tG\tB");
+
+  byte_ptr = (uint8_t *) mem;
+  
+  for (i = 0; i < num; i++)
+  {
+    sprintf(tmp_str, "0x%04x\t", (unsigned int) mem);
+    Serial.print(tmp_str);
+
+    for (j = 0; j < 3; j++)
+    {
+       sprintf(tmp_str, "0x%01x\t", *byte_ptr);
+       Serial.print(tmp_str);
+       byte_ptr++;
+    }
+    
+    Serial.println();
+
+  } 
+
+  Serial.println("=====================");
+}
+
+void memprint_CRGB( void *mem )
+{
+  memprint_CRGB( mem, 16 );
 }
--- memprint.h
+++ memprint.h
@@ -1,8 +1,17 @@
+
+
 void memprint_chars( char *mem );
-void memprint_chars( char *mem, int num_bytes );
+void memprint_chars( char *mem, int num_chars );
+void memprint_chars( void *mem );
+void memprint_chars( void *mem, int num_chars );
 
-void memprint_bytes( uint8_t *mem );
-void memprint_bytes( uint8_t *mem, int num_bytes );
+void memprint_bytes( unsigned char *mem );
+void memprint_bytes( unsigned char *mem, int num_bytes );
+void memprint_bytes( void *mem );
+void memprint_bytes( void *mem, int num_bytes );
 
-void memprint_CRGB( CRGB *mem );
-void memprint_CRGB( CRGB *mem, int num_bytes );
+void memprint_ints( int *mem );
+void memprint_ints( int *mem, int num_ints);
+
+void memprint_CRGB( void *mem );
+void memprint_CRGB( void *mem, int num_CRGBs );
